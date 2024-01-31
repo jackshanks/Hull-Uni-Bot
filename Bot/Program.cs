26aa0e4887bedc2a3cd6299086311c6e1025a54b
High <jacklshanks@gmail.com>
2024-01-31 15:32:33 +0000

    Bot goes online

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -15,15 +15,8 @@ public class Program
         _client = new DiscordSocketClient();
 
         _client.Log += Log;
-
-        //  You can assign your bot token to a string, and pass that in to connect.
-        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        
         var token = "MTIwMTg2NjkwNzMyMjY3OTMyNg.Gkp4H1.Cl4lxDuAtd7sBCA3LXLvf-ljNHCM2IwDh_JDMM";
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-        // var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
