f521caf4546bd8b8a6121ea5f895ee003b017772
High <jacklshanks@gmail.com>
2024-02-02 14:35:17 +0000

    dsada

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -34,7 +34,7 @@ public class Program
     {
         if (!msg.Author.IsBot)
         {
-            if (!(HasCharPrefix(msg,'!', 0) ||
+            if (!(HasStringPrefix(msg,'!', 0) ||
                 msg.Author.IsBot))
             {
                 msg.Channel.SendMessageAsync("Hello from your bot!");   
