7bc51806781e8d7900966910b69fcaea44a7f7aa
High <jacklshanks@gmail.com>
2024-02-02 13:29:05 +0000

    daad

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,58 +1,22 @@
-﻿using Bot;
+﻿using Discord;
 using Discord.Net;
-using Newtonsoft.Json;
-
-namespace DefaultNamespace;
-
-using Discord;
 using Discord.WebSocket;
-using Discord.Commands;
-
 
 public class Program
 {
     public static Task Main(string[] Args) => new Program().MainAsync();
-    
     private DiscordSocketClient _Client;
-	private SlashCommands _SlashCommands;
-
+    
     public async Task MainAsync()
     {
         _Client = new DiscordSocketClient();
-
-        _Client.Log += Log;
         
         var Token = Environment.GetEnvironmentVariable("BotToken");;
 
         await _Client.LoginAsync(TokenType.Bot, Token);
         await _Client.StartAsync();
-		await CreateGlobalCommandAsync();
-        //_Client.SlashCommandExecuted += _SlashCommands.SlashCommandExecuted;
 
-        // Block this task until the program is closed.
         await Task.Delay(-1);
     }
     
-    private Task Log(LogMessage Msg)
-    {
-        Console.WriteLine(Msg.ToString());
-        return Task.CompletedTask;
-    }
-
-    public async Task CreateGlobalCommandAsync()
-    {
-       var GlobalCommand = new SlashCommandBuilder();
-       GlobalCommand.WithName("ping");
-       GlobalCommand.WithDescription("Test your connection to the bot.");
-
-        try
-        {
-            await _Client.CreateGlobalApplicationCommandAsync(GlobalCommand.Build());
-       }catch (ApplicationCommandException Exception)
-       {
-            // Handle potential errors during command creation
-            Console.WriteLine(Exception.Message);
-       } 
-    }
-
 }
