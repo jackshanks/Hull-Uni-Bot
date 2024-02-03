37e4365dd5a7a52b34692fa645d9506a018e3ed7
High <jacklshanks@gmail.com>
2024-02-03 13:48:20 +0000

    dasdad

--- Bot/SlashCommandHandle.cs
+++ Bot/SlashCommandHandle.cs
@@ -53,7 +53,7 @@ public class SlashCommandHandle
             
             
             // Get the role ID of any role with $
-            ulong RoleId = User.Roles.FirstOrDefault(r => r.Name.StartsWith("$"))?.Id ?? 0;
+            ulong RoleId = User.Roles.FirstOrDefault(r => r.Name.StartsWith("/"))?.Id ?? 0;
 
             // Find the role to delete using SocketRole if an ID is found
             if (RoleId != 0)
@@ -64,8 +64,9 @@ public class SlashCommandHandle
             
             var Role = await Guild.CreateRoleAsync(RoleName, null, Color, false, false, null);
 
-
-            int DesiredPosition = 5;
+            ulong RoleId2 = User.Roles.FirstOrDefault(r => r.Name.StartsWith("$"))?.Id ?? 0;
+            SocketRole Role2 = Guild.Roles.FirstOrDefault(r => r.Id == RoleId);
+            int DesiredPosition = Role2.Position;
 
             await Role.ModifyAsync(p => p.Position = DesiredPosition);
 
