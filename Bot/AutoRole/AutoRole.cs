1ea0c09deaccc4ecd9577903cf0b0201342dbadb
jackshanks <jacklshanks@gmail.com>
2024-03-14 10:42:26 +0000

    Autorole

---
+++ Bot/AutoRole/AutoRole.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace;
+
+public class AutoRole : DiscordSocketClient
+{
+    protected override async Task OnMemberJoined(SocketGuildUser user)
+    {
+        var unverified = user.Guild.Roles.FirstOrDefault(x => x.Name == "Unverified");
+        if (unverified != null)
+        {
+            await user.AddRolesAsync(unverified);
+        }
+    }
+}
