4aa93cd14eb8f4d27ae430926e8de393c427b10d
jackshanks <jacklshanks@gmail.com>
2024-03-15 21:45:08 +0000

    Added lavalink

--- Bot/SlashCommands/Music.cs
+++ Bot/SlashCommands/Music.cs
@@ -37,7 +37,6 @@ namespace Bot.SlashCommands
         [SlashCommand("join", "Gets the bot to join the voice channel", runMode: Discord.Interactions.RunMode.Async)]
         public async Task JoinChannel(IVoiceChannel channel = null)
         {
-            // Get the audio channel
             channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
             if (channel == null)
             {
@@ -45,8 +44,7 @@ namespace Bot.SlashCommands
                     "User must be in a voice channel, or a voice channel must be passed as an argument.");
                 return;
             }
-
-            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            
             var audioClient = await channel.ConnectAsync();
         }
     }
