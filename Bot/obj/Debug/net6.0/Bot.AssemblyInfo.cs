4f502bfbf63ca40e59a53621d05d353ce0d9d9e6
jackshanks <jacklshanks@gmail.com>
2024-03-16 17:02:32 +0000

    Tells you it joined

--- Bot/obj/Debug/net6.0/Bot.AssemblyInfo.cs
+++ Bot/obj/Debug/net6.0/Bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c23190de0a77d68d7239800a4d6135f23fa9fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a0f7e8c1e23206d632d5449241068b516b3c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
