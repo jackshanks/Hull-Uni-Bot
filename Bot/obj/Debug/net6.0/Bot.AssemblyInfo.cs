3af25e51622ff870cc955ea4c1542902e08bfeca
jackshanks <jacklshanks@gmail.com>
2024-03-16 23:08:30 +0000

    Plz

--- Bot/obj/Debug/net6.0/Bot.AssemblyInfo.cs
+++ Bot/obj/Debug/net6.0/Bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3df92f70c8443457351740b188055ffe6d38a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6567db973c0b75e7cc55c0b8327d799fb734f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
