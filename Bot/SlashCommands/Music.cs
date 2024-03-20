de2bab3a87c8399ba536f33b1e2c30def6cbeb1f
jackshanks <jacklshanks@gmail.com>
2024-03-20 12:42:18 +0000

    OOps

--- Bot/SlashCommands/Music.cs
+++ Bot/SlashCommands/Music.cs
@@ -176,7 +176,7 @@ namespace Bot.SlashCommands
             }
         }
         
-        [SlashCommand("Pause", "Pause a song!")]
+        [SlashCommand("pause", "Pause a song!")]
         public async Task PauseAsync() {
             if (!_lavaNode.TryGetPlayer(Context.Guild, out var player))
             {
@@ -202,7 +202,7 @@ namespace Bot.SlashCommands
             }
         }
         
-        [SlashCommand("Resume", "Resume a song!")]
+        [SlashCommand("resume", "Resume a song!")]
         public async Task ResumeAsync() {
             if (!_lavaNode.TryGetPlayer(Context.Guild, out var player)) {
                 var embed = await _embedMaker.ErrorMessage("I'm not connected to a voice channel.");
@@ -227,7 +227,7 @@ namespace Bot.SlashCommands
             }
         }
         
-        [SlashCommand("Skip", "Skip a song!")]
+        [SlashCommand("skip", "Skip a song!")]
         public async Task SkipAsync() {
             if (!_lavaNode.TryGetPlayer(Context.Guild, out var player)) {
                 var embed = await _embedMaker.ErrorMessage("I'm not connected to a voice channel.");
